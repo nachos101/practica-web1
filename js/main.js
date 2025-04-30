f59764d8adedb1e2a489fb90a1773cba38e3ace3
nachos101 <nachostrau17@gmail.com>
2025-04-30 12:13:11 -0300

    update del form

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 "use strict"
 
-function saludar(){
-    
+function saludar(){  
     let N = prompt("Cual es tu nombre?");
     let Z = prompt ("Cual es tu apellido?");
     alert("Hola " + N + " " +Z);
     let variable = document.getElementById("123").innerHTML = N;
 }
+let X = document.getElementById("bt");
+X.addEventListener("click",saludar);
 
 let text1 = "Hola soy un ejemplo de js";
 let text2 = "Hola soy otro ejemplo";
@@ -16,11 +17,10 @@ let var1 = document.getElementById("1").innerHTML = text1;
 let var2 = document.getElementById("2").innerHTML = text2;
 let var3 = document.getElementById("3").innerHTML = text3;
 
-function boton1(){
+ function boton1(){
     let button1 = "El ultimo fue el 1"
     let var4 = document.getElementById("pp").innerHTML = button1;
 }
-
 function boton2(){
     let button1 = "El ultimo fue el 2"
     let var4 = document.getElementById("pp").innerHTML = button1;
@@ -30,6 +30,15 @@ function boton3(){
     let button1 = "El ultimo fue el 3"
     let var4 = document.getElementById("pp").innerHTML = button1;
 }
+
+let bt1 = document.getElementById("b1");
+bt1.addEventListener("click",boton1);
+
+let bt2 = document.getElementById("b2");
+bt2.addEventListener("click",boton2);
+
+let bt3 = document.getElementById("b3");
+bt3.addEventListener("click",boton3);
 
 function hola(){
     let ee = document.getElementById("8").value;
